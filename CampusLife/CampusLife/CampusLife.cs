ad4214f70bd2853aa877265b7e6c2f395a44cd32
geun25 <sas0898@naver.com>
2021-08-21 21:16:08 +0900

    .

---
+++ CampusLife/CampusLife/CampusLife.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusLife
+{
+    class CampusLife
+    {
+        static CampusLife singleton = null;
+
+        static CampusLife()
+        {
+            singleton = new CampusLife();
+        }
+        internal static CampusLife Singleton
+        {
+            get;
+        }
+        private CampusLife() // 개체 생성을 다른 형식에서 접근할 수 없게 private 접근 지정
+        {
+
+        }
+    }
+}
