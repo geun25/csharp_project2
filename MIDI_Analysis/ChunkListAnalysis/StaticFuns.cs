72ec5585af5813d9284579f9107bc71b1b37316e
geun25 <sas0898@naver.com>
2021-10-03 22:47:35 +0900

    midi

---
+++ MIDI_Analysis/ChunkListAnalysis/StaticFuns.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunkListAnalysis
+{
+    public static class StaticFuns
+    {
+        public static string GetString(int magic)
+        {
+            byte[] data = BitConverter.GetBytes(magic);
+            ASCIIEncoding en = new ASCIIEncoding();
+            return en.GetString(data);
+        }
+
+        // byte order = big / little endian
+        public static int ConvertHostorder(int data)
+        {
+            return IPAddress.NetworkToHostOrder(data);
+        }
+    }
+}
